8297bfe6f99bae5f0e9e2f5808c1b2a98fc69db7
vdsalex <alexandre.vandensteen@gmail.com>
2023-08-12 15:21:04 +0200

    optimize

--- src/main/kotlin/vds/com/domain/api/LoginResource.kt
+++ src/main/kotlin/vds/com/domain/api/LoginResource.kt
@@ -5,7 +5,6 @@ import io.ktor.server.application.Application
 import io.ktor.server.application.ApplicationEnvironment
 import io.ktor.server.application.call
 import io.ktor.server.auth.authenticate
-import io.ktor.server.auth.authentication
 import io.ktor.server.request.receive
 import io.ktor.server.response.respond
 import io.ktor.server.routing.get
@@ -13,7 +12,6 @@ import io.ktor.server.routing.post
 import io.ktor.server.routing.routing
 import org.koin.ktor.ext.inject
 import vds.com.domain.model.LoginUser
-import vds.com.domain.model.SimpleUser
 import vds.com.domain.spi.JwtHandler
 import vds.com.domain.spi.JwtUserHandler
 import vds.com.infra.model.UserDTO
